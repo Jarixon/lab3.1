0dfeec4c30f60802453b04d137fd933f8f385a34
Gabor L.D. Iaris-Filip <cti22a215@cst.uoradea.local>
2023-10-30 19:05:26 +0200

    Initial commit

---
+++ src/ro/uoradea/Main.java
@@ -0,0 +1,10 @@
+package ro.uoradea;
+
+public class Main {
+
+    public static void main(String[] args) {
+	      Dog dog1 = new Dog(num = "Rufus", i = 1);
+          Dog dog2 = new Dog(num = "Max", i = 2 );
+          System.out.println("Name: " + dog1.name + "age: " + dog1.age );
+    }
+}
